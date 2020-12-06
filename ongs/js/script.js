2820de95baafcd4cdc9ea6847aa6c69168954421
Isabela Gabriely <isabela.gabriely@hotmail.com>
2020-12-06 12:10:42 -0300

    adicionando todas as telas

---
+++ ongs/js/script.js
@@ -0,0 +1,9 @@
+function profile_settings (id) {
+	var display = document.getElementById(id).style.display;
+
+	if (display == 'none')
+		document.getElementById(id).style.display = 'block';
+
+	else
+		document.getElementById(id).style.display = 'none';
+}
